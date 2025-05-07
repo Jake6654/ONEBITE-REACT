0a1bfd8bbc24b61ca9739569c869f1b382b46dfa
Jake6654 <ugw2ad@virginia.edu>
2025-05-07 16:08:42 -0400

    Implement TodoList UI

---
+++ section08/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import List from './components/List'
+import Editor from './components/Editor'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <div className='App'>
+    <Header/>
+    <Editor/>
+    <List/>
+    </div>
+  
+
+  )
+}
+
+export default App
