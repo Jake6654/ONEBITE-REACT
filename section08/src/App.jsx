08f1ceab4413da487a7950127548e31c6ab0549e
Jake <ugw2ad@virginia.edu>
2025-05-11 16:36:33 -0400

    update codebase using useReducer

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,19 +1,98 @@
-import './App.css'
-import List from './components/List'
-import Editor from './components/Editor'
-import Header from './components/Header'
+import './App.css';
+import List from './components/List';
+import Editor from './components/Editor';
+import Header from './components/Header';
+import {useState, useRef, useReducer} from "react";
+
+const mockData = [ // 이런 고정된 mock data 는 컴포넌트 안에 있을 필요가 없으니 밖에다 설정
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+
+  {
+    id: 2,
+    isDone: false,
+    content: "운동하기",
+    date: new Date().getTime(),
+  }
+]
+
+function reducer(state, action){
+    switch(action.type){
+      case "CREATE":
+        return [action.data,...state] // action.data 을 추가 기존 데이터는 spread 연산자로 놔둠
+      case "UPDATE":
+        return state.map((item) => 
+          item.id === action.targetId 
+        ? {...item, isDone: !item.isDone} // targetId 가 있다면  item.isDone 을 ! 반전 시켜주기
+        : item
+      )
+      case "DELETE":
+        return state.filter(
+          (item) => item.id !== action.targetId);
+        // todos 에서 item id 가 action.targetId 아닌 것만 필터해서 새로운 투두 반환
+      default:
+        return state;
+      }
+}
 
 function App() {
+
+  const [todos, dispatch] = useReducer(reducer, mockData) // 초기값을 mockData 로 전달
+  const idRef =useRef(3); // id reference 객체 mock data 가 있기에 3부터 시작
+
+  const onCreate = (content) => {  // input 입력받은 값으로 Todo 추가하기
+      dispatch({
+        type:"CREATE",
+        data:{
+          id : idRef.current++,
+          isDone: false,
+          content : content,
+          date: new Date().getTime(),
+        }
+      });
+
+      setTodos([newTodo,...todos]) // spread 문법으로 기존 todos 에 newTodo 추가
+      // state 변수인 todos 는 무조건 setTodos 으로 값을 설정 변경해야함
+  }
+
+  const onUpdate = (targetId) => {
+    // todos State의 값들 중에 
+    // targetId 와 일치하는 id를 갖는 투두 아이템의 isDone 변경
+    dispatch({
+      type:"UPDATE",
+      targetId : targetId
+    });    
+  
+};
+
+  const onDelete = (targetId) => {
+    // 인수: todos 배열에서 targetId와 일치하는 id 를 갖는 요소만 삭제한 새로운 배열
+       dispatch({
+        type:"DELETE",
+        targetId : targetId
+       });
+  };
 
   return (
     <div className='App'>
     <Header/>
-    <Editor/>
-    <List/>
+    <Editor onCreate = {onCreate}/> 
+    <List todos = {todos} onUpdate ={onUpdate} onDelete ={onDelete}/>
     </div>
-  
 
-  )
+  );
 }
+
 
 export default App
--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,16 +1,41 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import {useState} from "react";
 
 
-const List = () => {
+const List = ({todos, onUpdate, onDelete}) => {
+    const [search, setSearch] = useState("");
+
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    };
+
+    const getFilteredData = () =>{
+        if(search === ""){
+            return todos;
+        }
+        return todos.filter((todo) => // todos 배열에 filter 메서드 실행
+             todo.content
+             .toLowerCase()
+             .includes(search.toLowerCase()));
+              // 각 배열의 값에 search(사용자가 찾고자 하는 값을 갖고 있는지) 필터링
+              // ex todo.content) "리액트 공부하기" 있다면 ture 을 반환
+    }
+
+    const filteredTodos = getFilteredData();
+
+
     return (
     <div>
         <h4>Todo List🌱</h4>
-        <input placeholder="검색어를 입력하세요" />
+        <input 
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요" />
         <div className="todos_wrapper">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            {filteredTodos.map((todo) =>{
+                return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} onDelete={onDelete}/>;
+            })}
         </div>
     </div>
     );
